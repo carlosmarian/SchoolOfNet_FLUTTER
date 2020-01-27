da9cfce328d1c1f844fb099f87927cc50a4f1dba
Carlos H. Marian <carlos.marian@amcom.com.br>
2020-01-27 13:13:19 -0300

    TEst in basic project

--- Flutter_Para_Iniciantes/son_counter/lib/main.dart
+++ Flutter_Para_Iniciantes/son_counter/lib/main.dart
@@ -18,9 +18,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightGreen,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page Teste'),
     );
   }
 }
@@ -53,7 +53,12 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      
+      if(_counter >= 5){
+        _counter --;
+      }else{
+        _counter++;
+      }
     });
   }
 
