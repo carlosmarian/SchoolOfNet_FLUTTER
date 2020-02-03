d8d17f8731032334265141de82dc7b50f0b3f0d8
Carlos H. Marian <carlos.marian@amcom.com.br>
2020-02-03 19:13:55 -0300

    choice image increment curses

--- Flutter_Para_Iniciantes/son_counter/lib/pages/homePage.dart
+++ Flutter_Para_Iniciantes/son_counter/lib/pages/homePage.dart
@@ -7,6 +7,16 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _cursos = 0;
+  int _selectedImage = 0;
+
+  var _images = [
+    'images/1.png',
+    'images/2.png',
+    'images/3.png',
+    'images/4.png',
+    'images/5.png',
+    'images/6.png'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +25,7 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           //Aceita inumeros filhos
           children: <Widget>[
-            Image.asset('images/1.png', height: 100.0,),
+            Image.asset(_images[_selectedImage], height: 100.0,),
             Text('Cursos', 
                   textAlign: TextAlign.center, 
                   style: Theme.of(context).textTheme.display4,),
@@ -27,6 +37,16 @@ class _HomePageState extends State<HomePage> {
                     child: FloatingActionButton(onPressed: (){
                       setState(() {
                         _cursos++;
+                        
+                        var selectedImage = _cursos ~/ 2;
+
+                        if(selectedImage <= 4){
+                          _selectedImage = selectedImage;
+                        }
+
+                        if(selectedImage > 4){
+                          _selectedImage = 5;
+                        }
                       });
                     },
                     child: Icon(Icons.add),),
