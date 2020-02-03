43fc429b61a2968d45d5207d04722eb28a9e24c9
Carlos H. Marian <carlos.marian@amcom.com.br>
2020-02-03 18:07:42 -0300

    Using StatefulWidget and StatelessWidget

--- Flutter_Para_Iniciantes/son_counter/lib/main.dart
+++ Flutter_Para_Iniciantes/son_counter/lib/main.dart
@@ -2,9 +2,29 @@ import 'package:flutter/material.dart';
 
 void main(){
 
-  runApp(new MaterialApp(
+  runApp(new MyApp());
+
+}
+
+
+class MyApp extends StatelessWidget{
+  Widget build(BuildContext context){
+    return new MaterialApp(
     title: 'SON contador de cursos',
-    home: new Container(
+    home: new HomeApp(),
+  );
+  }
+}
+
+class HomeApp extends StatefulWidget {
+  @override
+  _HomeAppState createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
       //Container pai
       color: Color.fromARGB(255, 255, 255, 255),
       //child : Aceita somente 1 item filho      
@@ -17,7 +37,6 @@ void main(){
           Text('Novo teste 2', textAlign: TextAlign.left)
         ],
       ),
-    )
-  ));
-
+    );
+  }
 }
