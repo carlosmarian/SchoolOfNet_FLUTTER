3657ea36fa351c9901e42fd86d728efb5ffee5ac
Carlos H. Marian <carlos.marian@amcom.com.br>
2020-02-03 19:28:06 -0300

    Configure page SchoolOfNet, add dependences.

--- Flutter_Para_Iniciantes/son_counter/README.md
+++ Flutter_Para_Iniciantes/son_counter/README.md
@@ -14,3 +14,12 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+Para abrir pagina, é necessário usar um complemento extendo, será usado o "url_launcher". Site: https://pub.dev/packages/url_launche
+
+para isso deve add a depencia no "pubspec.yaml" 
+
+url_launcher: ^5.4.1
+
+e depois salvar que a IDE já vai executar o "flutter pub get"
--- Flutter_Para_Iniciantes/son_counter/lib/pages/schoolPage.dart
+++ Flutter_Para_Iniciantes/son_counter/lib/pages/schoolPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SchoolPage extends StatefulWidget {
   @override
@@ -9,12 +10,21 @@ class _SchoolPageState extends State<SchoolPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text('Visite:', textAlign: TextAlign.center,),
+        Text('Visite:', 
+          textAlign: TextAlign.center, 
+          style: Theme.of(context).textTheme.display3,),
         RaisedButton(
           child: Text('schoolofnet.com.br', textAlign: TextAlign.center),
+          onPressed: (){
+            //Ação de visitar uma pagina no navegador
+            launch('https://www.schoolofnet.com');
+          },
         )
       ],
     );
   }
+  
 }
+
